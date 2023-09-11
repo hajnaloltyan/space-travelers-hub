8b1b72a446426999cf316e03832006ac81fc95de
Hajnalka Oltyan <hajnalka.oltyan@gmail.com>
2023-09-11 23:21:17 +0200

    Fix routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {
-  Route, Routes, NavLink,
+  HashRouter as Router, Routes, Route, NavLink,
 } from 'react-router-dom';
 import Dragons from './components/Dragons';
 import Missions from './components/Missions/Missions';
@@ -23,13 +23,13 @@ function App() {
                   <NavLink to="/" className={(navData) => (navData.isActive ? 'active-style' : 'none')}>Rockets</NavLink>
                 </li>
                 <li className="nav-item">
-                  <NavLink to="/missions" className={(navData) => (navData.isActive ? 'active-style' : 'none')}>Missions</NavLink>
+                  <NavLink to="missions" className={(navData) => (navData.isActive ? 'active-style' : 'none')}>Missions</NavLink>
                 </li>
                 <li className="nav-item">
-                  <NavLink to="/dragons" className={(navData) => (navData.isActive ? 'active-style' : 'none')}>Dragons</NavLink>
+                  <NavLink to="dragons" className={(navData) => (navData.isActive ? 'active-style' : 'none')}>Dragons</NavLink>
                 </li>
                 <li className="nav-item">
-                  <NavLink to="/myprofile" className={(navData) => (navData.isActive ? 'active-style' : 'none')}>My Profile</NavLink>
+                  <NavLink to="myprofile" className={(navData) => (navData.isActive ? 'active-style' : 'none')}>My Profile</NavLink>
                 </li>
               </ul>
             </div>
@@ -40,12 +40,15 @@ function App() {
           </button>
         </div>
       </nav>
-      <Routes>
-        <Route path="/" element={<Rockets />} />
-        <Route path="/missions" element={<Missions />} />
-        <Route path="/dragons" element={<Dragons />} />
-        <Route path="/myprofile" element={<MyProfile />} />
-      </Routes>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Rockets />}>
+            <Route path="missions" element={<Missions />} />
+            <Route path="dragons" element={<Dragons />} />
+            <Route path="myprofile" element={<MyProfile />} />
+          </Route>
+        </Routes>
+      </Router>
     </>
   );
 }
